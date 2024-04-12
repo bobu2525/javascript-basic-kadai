265a38b4c7964ff29039578202c24bdbcb1fa827
bobu2525 <108675227+bobu2525@users.noreply.github.com>
2024-04-12 10:11:41 +0900

    課題005
    アップロード

---
+++ kadi_005/bmi.js
@@ -0,0 +1,14 @@
+
+
+// 変数の宣言
+let userHeight;
+let userWeight;
+let userBmi;
+
+// 値の代入
+userHeight = 1.7;
+userWeight = 68;
+
+userBmi = userWeight/(userHeight*userHeight);
+
+console.log(userBmi);
