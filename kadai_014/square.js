f967fd81c41dc16840d4dc456da4bddee4a685b6
bobu2525 <108675227+bobu2525@users.noreply.github.com>
2024-04-14 18:16:12 +0900

    Add files via upload

---
+++ kadai_014/square.js
@@ -0,0 +1,14 @@
+// 変数numを定義
+// 変数numにランダムな整数を代入
+
+let num = Math.floor(Math.random()*30)+1;
+
+// 変数の値を出力する（確認用）
+console.log(num);
+
+
+const double = (by) => {
+  console.log(by*2);
+};
+
+double(num);
